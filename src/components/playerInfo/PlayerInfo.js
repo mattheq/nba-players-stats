3619f527298a7ea311dd4e2858a892d5af4ed0d0
Mateusz Grzyb <mat.grz1@gmail.com>
2020-03-26 08:21:40 +0100

    displaying player team in PlayerInfo component

--- src/components/playerInfo/PlayerInfo.js
+++ src/components/playerInfo/PlayerInfo.js
@@ -5,7 +5,7 @@ export default function PlayerInfo({ player }) {
     return (
         <div>
             <h1 className="player-name">{player.first_name} {player.last_name}</h1>
-            <h3 className="player-team">{player.first_name} {player.last_name}</h3>
+            <h3 className="player-team">{player.team.full_name}</h3>
         </div>
     );
 }
