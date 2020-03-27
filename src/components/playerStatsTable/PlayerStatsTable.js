fa26ad76f161572548f2c06e45f598a3b9718d8b
Mateusz Grzyb <mat.grz1@gmail.com>
2020-03-27 09:50:59 +0100

    added labels for player stats

--- src/components/playerStatsTable/PlayerStatsTable.js
+++ src/components/playerStatsTable/PlayerStatsTable.js
@@ -3,9 +3,30 @@ import './PlayerStatsTable.css';
 
 export default function PlayerStatsTable({ stats, isLoading }) {
 
-    let playerStats = Object.keys(stats).map(key => {
+    const statsLabels = {
+        'fgm': 'field goals made',
+        'fga': 'field goals attempted',
+        'fg3m': '3-PT field goals made',
+        'fg3a': '3-PT field goals attempted',
+        'ftm': 'free throws made',
+        'fta': 'free throws attempted',
+        'oreb': 'offensive rebounds',
+        'dreb': 'defensive rebounds',
+        'reb': 'rebounds',
+        'ast': 'assists',
+        'stl': 'steals',
+        'blk': 'blocks',
+        'turnover': 'turnovers',
+        'pf': 'personal fouls',
+        'pts': 'points',
+        'fg_pct': 'field goals percentage',
+        'fg3_pct': '3-PT goals percentage',
+        'ft_pct': 'free throws percentage'
+    };
+
+    let playerStats = Object.keys(stats).filter(key => key in statsLabels).map(key => {
         return <div className="stat-item" key={key}>
-            <div className="stat-name">{key} </div>
+            <div className="stat-name">{statsLabels[key]} </div>
             <div className="stat-value">{stats[key]}</div>
         </div>;
     });
