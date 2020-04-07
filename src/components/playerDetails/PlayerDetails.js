066d9a519b6c5cbd2c5b400cb3cd465f0b44e897
Mateusz Grzyb <mat.grz1@gmail.com>
2020-04-07 11:31:50 +0200

    removed unused state

--- src/components/playerDetails/PlayerDetails.js
+++ src/components/playerDetails/PlayerDetails.js
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './PlayerDetails.css';
 
 export default function PlayerDetails({ player }) {
-    let [isLoading, setIsLoading] = useState(false);
-
     return (
-        <>
-            {!isLoading &&
-                <>
-                <div className="player-info-container">
-                    <div className="player-info">
-                        <h1 className="player-name">{player.first_name} {player.last_name}</h1>
-                        <h3 className="player-team">{player.team.full_name}</h3>
-                    </div>
-                    <img src="shirt.svg" alt="shirt" />
-                </div>
-                </>
-            }
-        </>
+        <div className="player-info-container">
+            <div className="player-info">
+                <h1 className="player-name">{player.first_name} {player.last_name}</h1>
+                <h3 className="player-team">{player.team.full_name}</h3>
+            </div>
+            <img src="shirt.svg" alt="shirt" />
+        </div>
     );
 }
