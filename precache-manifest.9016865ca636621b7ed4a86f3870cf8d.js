d747b7e987bcb1c43af6682d0b2d2e3343acf923
Mateusz Grzyb <mat.grz1@gmail.com>
2020-04-09 10:17:27 +0200

    Updates

---
+++ precache-manifest.9016865ca636621b7ed4a86f3870cf8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ac64d6b52c6971ddd8775ae389579a",
+    "url": "/nba-players-stats/index.html"
+  },
+  {
+    "revision": "c93a58b3fb0cbfa896ad",
+    "url": "/nba-players-stats/static/css/main.643b2889.chunk.css"
+  },
+  {
+    "revision": "51acc18b0df5a07b5eb6",
+    "url": "/nba-players-stats/static/js/2.64079049.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/nba-players-stats/static/js/2.64079049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93a58b3fb0cbfa896ad",
+    "url": "/nba-players-stats/static/js/main.167d5c02.chunk.js"
+  },
+  {
+    "revision": "4c248e5234d44e48ff20",
+    "url": "/nba-players-stats/static/js/runtime-main.f6447f48.js"
+  }
+]);
